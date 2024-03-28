205e297d26d3e404df000ebf64211244d5414a18
Mackay89 <manotomackay@gmail.com>
2024-03-28 05:26:51 -0700

    task

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+/***
+ * binary_tree_size - Measure the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the size
+ * Return: The size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, right = 0, left = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_size(tree->left);
+		right = binary_tree_size(tree->right);
+		size = right + left + 1;
+	}
+	return (size);
+}
--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_delete - Deletes an entire binary tree
- * @tree: Pointer to the root node of the tree to deleete
+ * @tree: Pointer to the root node of the tree to delete
  * return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
@@ -14,11 +14,8 @@ void binary_tree_delete(binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree != NULL)
-		{
-			binary_tree_delete(tree->left);
-			binary_tree_delete(tree->right);
-		}
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 		free(tree);
 	}
 }
