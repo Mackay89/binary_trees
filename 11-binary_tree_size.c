077500e413231df4febe2f0dd0b314735d5d4e56
Mackay89 <manotomackay@gmail.com>
2024-03-28 10:40:34 -0700

    task

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,18 +3,15 @@
 
 /***
  * binary_tree_size - Measures the size of a binary tree
- * @tree: Pointer to the root node of the tree to measure the size
- * Return: The size of the tree
+ * @tree: A pointer to the root node of the tree to measure the size
+ *
+ * Return: The size of the tree, or 0 if tree is NULL.
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, right = 0, left = 0;
+	if (tree == NULL)
+		return (0);
 
-	if (tree)
-	{
-		left = binary_tree_size(tree->left);
-		right = binary_tree_size(tree->right);
-		size = right + left + 1;
-	}
-	return (size);
-}
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,9 +8,6 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
-
 	if (tree == NULL)
 		return (0);
 
@@ -18,10 +15,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (1);
 
 	if (tree->left && tree->right)
-	{
-		left = binary_tree_is_full(tree->left);
-		right = binary_tree_is_full(tree->right);
-	}
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
 	return (0);
 }
