c57efd12dd5f6ff78671aa7a9468bf3f72265ed0
Mackay89 <manotomackay@gmail.com>
2024-03-27 22:46:07 -0700

    task

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,8 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-
+	
+	
 	if (parent == NULL)
 		return NULL;
 
@@ -20,13 +21,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return NULL;
 
+
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
+
+
 	parent->left =new_node;
 
 
 	return new_node;
+
 }
